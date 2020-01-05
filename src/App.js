adc9c55b3bee907335675bedb34a13512184b22e
AKS <aks000203@gmail.com>
2020-01-05 11:47:00 +0530

    Final commit: by AKS

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Atul from './component/atul';
+
+function App() {
+  return (
+    <div className="App">
+      dfghjk
+      <Atul />
+    </div>
+  );
+}
+
+export default App;
